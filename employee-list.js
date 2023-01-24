93f00fe9bed7528d9525515b0965fbe8e344b145
joncoder <joncoder.kamite@gmail.com>
2023-01-24 15:17:03 +0100

    refactoring si localStorage -> empty alors envoi le mock employes.json

--- employee-list.js
+++ employee-list.js
@@ -1,5 +1,6 @@
-$(function() {
+/*$(function() {
     const employees = JSON.parse(localStorage.getItem('employees'));
+    const employeesJson =
 
     $('#employee-table').DataTable({
         data: employees,
@@ -16,3 +17,35 @@ $(function() {
         ]
     });
 });
+*/
+$(document).ready(function() {
+    let employees = JSON.parse(localStorage.getItem('employees'));
+    if (!employees) {
+        fetch('./Employees.json')
+            .then(response => response.json())
+            .then(employeesJsonData => {
+                employees = employeesJsonData;
+                localStorage.setItem('employees', JSON.stringify(employees));
+                initDataTable(employees);
+            });
+    } else {
+        initDataTable(employees);
+    }
+});
+
+function initDataTable(employees) {
+    $('#employee-table').DataTable({
+        data: employees,
+        columns: [
+            { title: 'First Name', data: 'firstName' },
+            { title: 'Last Name', data: 'lastName' },
+            { title: 'Start Date', data: 'startDate' },
+            { title: 'Department', data: 'department' },
+            { title: 'Date of Birth', data: 'dateOfBirth' },
+            { title: 'Street', data: 'street' },
+            { title: 'City', data: 'city' },
+            { title: 'State', data: 'state' },
+            { title: 'Zip Code', data: 'zipCode' },
+        ]
+    });
+}
